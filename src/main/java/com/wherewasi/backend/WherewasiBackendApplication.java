852f1a584c41488cbb6ee2a63913d8962e66bc59
Jackson Barr <jacksonbarr2021@gmail.com>
2025-07-14 12:19:26 -0400

    Initial commit

---
+++ src/main/java/com/wherewasi/backend/WherewasiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wherewasi.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WherewasiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WherewasiBackendApplication.class, args);
+	}
+
+}
