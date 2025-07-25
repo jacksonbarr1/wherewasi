8f5a53597146b7d4ddbd35b941287abf1dfe987c
Jackson Barr <jacksonbarr2021@gmail.com>
2025-07-25 13:56:41 -0400

    Ensure idempotency for daily ingestion job

--- src/main/java/com/wherewasi/backend/mapper/EpisodeMapper.java
+++ src/main/java/com/wherewasi/backend/mapper/EpisodeMapper.java
@@ -5,6 +5,7 @@ import com.wherewasi.backend.dto.tmdb.TMDBShowDTO;
 import com.wherewasi.backend.entity.Episode;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring", uses = {
         DateMapper.class
@@ -14,4 +15,7 @@ public interface EpisodeMapper {
 
     @Mapping(target = "airDate", source = "airDate", qualifiedByName = "stringToLocalDate")
     Episode dtoToEpisode(TMDBEpisodeDTO episodeDTO);
+
+    @Mapping(target = "airDate", source = "airDate", qualifiedByName = "stringToLocalDate")
+    void updateEpisodeFromDTO(TMDBEpisodeDTO episodeDTO, @MappingTarget Episode episode);
 }
--- src/main/java/com/wherewasi/backend/service/TMDBIngestionServiceImpl.java
+++ src/main/java/com/wherewasi/backend/service/TMDBIngestionServiceImpl.java
@@ -17,17 +17,14 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Service
 public class TMDBIngestionServiceImpl implements TMDBIngestionService {
 
     // TODO: Use generics to decouple processing logic from specific nested DTO types
-    // TODO: Implement schedule for calling ingestion job
 
     private static final Logger logger = LoggerFactory.getLogger(TMDBIngestionServiceImpl.class);
 
@@ -68,41 +65,79 @@ public class TMDBIngestionServiceImpl implements TMDBIngestionService {
 
 
     @Override
-    @Scheduled(cron = "0 19 20 * * *", zone="America/New_York")
+    @Scheduled(cron = "0 53 13 * * *", zone="America/New_York")
     public void dailyIngestionJob() {
-        // Get exported id file, filter, sort
-        Stream<TMDBShowIdExportDTO> processedStream = processDatedIdFile(LocalDate.now(ZoneId.of("America/New_York")));
+        logger.info("Starting daily TMDB ingestion job.");
+        try {
+            Stream<TMDBShowIdExportDTO> processedStream = processDatedIdFile(LocalDate.now
+                    (ZoneId.of("America/New_York")));
 
-        // Retrieve and persist details for each show in the stream
-        processedStream.forEach(tmdbShowIdExportDTO -> {
-            processShow(tmdbShowIdExportDTO.getId());
-        });
+            processedStream.forEach(tmdbShowIdExportDTO -> {
+                try {
+                    processShow(tmdbShowIdExportDTO.getId());
+                } catch (Exception e) {
+                    logger.error("Error processing show with ID {}: {}", tmdbShowIdExportDTO.getId(), e.getMessage(), e);
+                }
+            });
+            logger.info("Daily TMDB ingestion job completed successfully.");
+        } catch (Exception e) {
+            logger.error("Daily TMDB ingestion job failed: {}", e.getMessage(), e);
+        }
     }
 
     private Stream<TMDBShowIdExportDTO> processDatedIdFile(LocalDate date) {
-        // Returns an unfiltered, unsorted stream of IDs for every show on TMDB
-        Stream<TMDBShowIdExportDTO> showIdStream = tmdbApiClient.downloadAndStreamShowIds(date);
-        logger.info("Successfully retrieved show IDs for date: {}", date);
+        logger.info("Attempting to retrieve show IDs for date: {}", date);
+        try {
+            Stream<TMDBShowIdExportDTO> showIdStream = tmdbApiClient.downloadAndStreamShowIds(date);
+            logger.info("Successfully retrieved show IDs for date: {}", date);
 
-        logger.info("Filtering and sorting retrieved show IDs");
-        return showIdStream
-                .filter(dto -> StringUtil.isEnglishOnly(dto.getName()))
-                .sorted(Comparator
-                        .comparing(TMDBShowIdExportDTO::getPopularity, Comparator.nullsLast(Comparator.reverseOrder()))
-                        .thenComparing(TMDBShowIdExportDTO::getName, Comparator.nullsLast(String.CASE_INSENSITIVE_ORDER)));
+            logger.info("Filtering and sorting retrieved show IDs");
+            return showIdStream
+                    .filter(dto -> {
+                        if (StringUtil.isEnglishOnly(dto.getName())) {
+                            return true;
+                        } else {
+                            logger.debug("Skipping show ID {} with non-English name: {}", dto.getId(), dto.getName());
+                            return false;
+                        }
+                    })
+                    .sorted(Comparator
+                            .comparing(TMDBShowIdExportDTO::getPopularity, Comparator.nullsLast(Comparator.reverseOrder()))
+                            .thenComparing(TMDBShowIdExportDTO::getName, Comparator.nullsLast(String.CASE_INSENSITIVE_ORDER)));
+        } catch (Exception e) {
+            logger.error("Failed to retrieve show IDs for date {}: {}", date, e.getMessage(), e);
+            return Stream.empty();
+        }
     }
 
     @Transactional
     public void processShow(Long showId) {
+        // TODO: Method too long
 
-        // TODO: Determine if show already exists in the database
-        // If show exists and show status is ended, skip processing
-        // If show exists and show status is not ended but fetched show episode count is the same as the existing show,
-        //     skip processing
+        logger.info("Initiating processing for show with TMDB ID: {}", showId);
+        Optional<Show> existingShowOptional = showRepository.findById(showId);
 
-        // TODO: Skip talk shows, news shows, and other non-episodic content because holy crap
+        if (existingShowOptional.isPresent()) {
+            Show existingShow = existingShowOptional.get();
+            if (existingShow.getStatus() == "Ended") {
+                logger.info("Skipping processing for show ID {} as it has ended", showId);
+                return;
+            }
 
-        logger.info("Processing show with ID: {}", showId);
+            Optional<TMDBShowDTO> showDTOOptionalForCheck = tmdbApiClient.getTMDBShowDetails(showId);
+            if (showDTOOptionalForCheck.isPresent()) {
+                TMDBShowDTO latestShowDTO = showDTOOptionalForCheck.get();
+                if (existingShow.getNumberOfEpisodes() != null &&
+                    existingShow.getNumberOfEpisodes().equals(latestShowDTO.getNumberOfEpisodes())) {
+                    logger.info("Skipping processing for show ID {} as the episode count has not changed", showId);
+                    return;
+                }
+            } else {
+                logger.warn("Could not retrieve show details for ID {} to check episode count. " +
+                        "Proceeding with full ingestion", showId);
+            }
+        }
+
         Optional<TMDBShowDTO> showDTOOptional = tmdbApiClient.getTMDBShowDetails(showId);
 
         if (showDTOOptional.isEmpty()) {
@@ -110,77 +145,107 @@ public class TMDBIngestionServiceImpl implements TMDBIngestionService {
             return;
         }
 
-
-        TMDBShowDTO showDTO = showDTOOptional.get();
-        logger.info("Successfully retrieved details for show ID: {} ({})", showDTO.getId(), showDTO.getName());
-
-        Show show = showRepository.findById(showId).orElseGet(Show::new);
-
-        showMapper.updateShowFromDTO(showDTO, show);
-        show.setId(showDTO.getId());
-        show.setLastApiFetchTimestamp(LocalDateTime.now());
-
-        // Map nested DTOs to entities, persist and set them in the show entity
-        processGenres(showDTO.getGenres(), show);
-        processCreators(showDTO.getCreatedBy(), show);
-        processNetworks(showDTO.getNetworks(), show);
-        processSeasons(showDTO.getSeasons(), show);
-
-        // Iterate through each season and call `processEpisode` for episode numbers [1, season.episodeCount]
-
-        for (Season season : show.getSeasons()) {
-            if (season.getEpisodes() == null || season.getEpisodes().isEmpty()) {
-                logger.info("Season {} for show ID {} has no episodes, processing episodes now", season.getSeasonNumber(), show.getId());
-                List<Episode> episodes = new ArrayList<>();
-                season.setEpisodes(episodes);
-            }
-            ingestEpisodesBySeason(show, season);
-            seasonRepository.save(season);
+        if (showDTOOptional.get().getType().equals("Talk Show")) {
+            logger.info("Skipping processing for show ID {} as it is a Talk Show", showId);
+            return;
         }
 
-        showRepository.save(show);
+        TMDBShowDTO showDTO = showDTOOptional.get();
+
+        // TODO: Implement business rule to skip talk shows, news, etc
+
+        // If the show is already in the database, update; otherwise create a new Show entity
+        Show show = existingShowOptional.orElseGet(Show::new);
+        showMapper.updateShowFromDTO(showDTO, show);
+        show.setId(showDTO.getId());
+        show.setLastApiFetchTimestamp(LocalDateTime.now(ZoneId.of("America/New_York")));
+
+        try {
+            processGenres(showDTO.getGenres(), show);
+            processCreators(showDTO.getCreatedBy(), show);
+            processNetworks(showDTO.getNetworks(), show);
+            processSeasons(showDTO.getSeasons(), show);
+
+            for (Season season : show.getSeasons()) {
+                ingestEpisodesBySeason(season, show);
+            }
+
+            showRepository.save(show);
+            logger.info("Successfully processed and saved show {} with ID: {}", show.getName(), show.getId());
+        } catch (Exception e) {
+            logger.error("Error processing show ID {}: {}", showId, e.getMessage(), e);
+        }
     }
 
-    private void ingestEpisodesBySeason(Show show, Season season) {
+
+    private void ingestEpisodesBySeason(Season season, Show show) {
         if (season.getEpisodeCount() == null || season.getEpisodeCount() <= 0) {
             logger.warn("Season {} for show ID {} has no episodes to process", season.getSeasonNumber(), show.getId());
             return;
         }
 
-        logger.info("Processing episodes for show ID {} in season {}", show.getId(), season.getSeasonNumber());
+        logger.info("Processing episodes for show ID {} ({}) in season {}", show.getId(), show.getName(), season.getSeasonNumber());
+
+        Set<Integer> existingEpisodeNumbers = season.getEpisodes().stream()
+                .map(Episode::getEpisodeNumber)
+                .collect(Collectors.toSet());
 
         for (int episodeNumber = 1; episodeNumber <= season.getEpisodeCount(); episodeNumber++) {
+            if (existingEpisodeNumbers.contains(episodeNumber)) {
+                logger.debug("S{}E{} for show ID {} already exists. Skipping.",
+                        season.getSeasonNumber(), episodeNumber, show.getId());
+                continue;
+            }
             processEpisode(show.getId(), season, episodeNumber);
         }
     }
 
     private void processEpisode(Long showId, Season season, Integer episodeNumber) {
-        logger.info("Retrieving S{}E{} details for show ID {}", season.getSeasonNumber(), episodeNumber, showId);
+        logger.info("Attempting to retrieve S{}E{} details for show ID {}",
+                season.getSeasonNumber(), episodeNumber, showId);
 
-        // Retrieve episode DTO from TMDB API
-        Optional<TMDBEpisodeDTO> episodeDTOOptional = tmdbApiClient.getTMDBEpisodeDetails(showId,
-                season.getSeasonNumber(), episodeNumber);
+        Optional<TMDBEpisodeDTO> episodeDTOOptional;
 
-        // Map to Episode entity
+        try {
+            episodeDTOOptional = tmdbApiClient.getTMDBEpisodeDetails(showId, season.getSeasonNumber(), episodeNumber);
+        } catch (Exception e) {
+            logger.error("Error retrieving S{}E{} details for show ID {}: {}",
+                    season.getSeasonNumber(), episodeNumber, showId, e.getMessage());
+            return;
+        }
 
         if (episodeDTOOptional.isEmpty()) {
-            logger.warn("Failed to retrieve details for S{}E{} of show ID {}", season.getSeasonNumber(), episodeNumber, showId);
+            logger.warn("Failed to retrieve details for S{}E{} of show ID {}",
+                    season.getSeasonNumber(), episodeNumber, showId);
             return;
         }
 
         TMDBEpisodeDTO episodeDTO = episodeDTOOptional.get();
 
-        Episode episode = episodeMapper.dtoToEpisode(episodeDTO);
+        Optional<Episode> existingEpisodeOptional = episodeRepository.findBySeasonAndEpisodeNumber(season, episodeNumber);
+        Episode episode = existingEpisodeOptional.orElseGet(Episode::new);
 
-        // Add to season's episode list
+
+        episodeMapper.updateEpisodeFromDTO(episodeDTO, episode);
         episode.setSeason(season);
-        List<Episode> episodes = season.getEpisodes();
-        episodes.add(episode);
-        season.setEpisodes(episodes);
 
-        logger.info("Saving episode: {}", episode);
+        if (existingEpisodeOptional.isEmpty()) {
+            season.getEpisodes().add(episode);
+            logger.info("Added new episode S{}E{} to season {} of show ID {}",
+                    episode.getSeason().getSeasonNumber(), episode.getEpisodeNumber(), season.getId(), showId);
+        } else {
+            logger.info("Updated existing episode S{}E{} for season {} of show ID {}",
+                    episode.getSeason().getSeasonNumber(), episode.getEpisodeNumber(), season.getId(), showId);
+        }
 
-        episodeRepository.save(episode);
+        try {
+            episodeRepository.save(episode);
+            logger.debug("Successfully saved episode S{}E{} for show ID {}",
+                    episode.getSeason().getSeasonNumber(), episode.getEpisodeNumber(), showId);
+        } catch (Exception e) {
+            logger.error("Error saving episode S{}E{} for show ID {}: {}",
+                    episode.getSeason().getSeasonNumber(), episode.getEpisodeNumber(), showId, e.getMessage());
+        }
     }
 
     private void processGenres(List<TMDBShowDTO.TMDBGenreDTO> genreDTOs, Show show) {
@@ -193,10 +258,7 @@ public class TMDBIngestionServiceImpl implements TMDBIngestionService {
 
         for (TMDBShowDTO.TMDBGenreDTO genreDTO : genreDTOs) {
             Genre genre = genreRepository.findById(genreDTO.getId())
-                    .orElseGet(() -> {
-                        Genre newGenre = genreMapper.toGenre(genreDTO);
-                        return genreRepository.save(newGenre);
-                    });
+                    .orElseGet(() -> genreMapper.toGenre(genreDTO));
             currentGenres.add(genre);
         }
 
@@ -213,10 +275,7 @@ public class TMDBIngestionServiceImpl implements TMDBIngestionService {
 
         for (TMDBShowDTO.TMDBCreatorDTO creatorDTO : creatorDTOs) {
             Creator creator = creatorRepository.findById(creatorDTO.getId())
-                    .orElseGet(() -> {
-                        Creator newCreator = creatorMapper.toCreator(creatorDTO);
-                        return creatorRepository.save(newCreator);
-                    });
+                    .orElseGet(() -> creatorMapper.toCreator(creatorDTO));
             currentCreators.add(creator);
         }
 
@@ -233,10 +292,7 @@ public class TMDBIngestionServiceImpl implements TMDBIngestionService {
 
         for (TMDBShowDTO.TMDBNetworkDTO networkDTO : networkDTOs) {
             Network network = networkRepository.findById(networkDTO.getId())
-                    .orElseGet(() -> {
-                        Network newNetwork = networkMapper.toNetwork(networkDTO);
-                        return networkRepository.save(newNetwork);
-                    });
+                    .orElseGet(() -> networkMapper.toNetwork(networkDTO));
             currentNetworks.add(network);
         }
 
@@ -252,13 +308,12 @@ public class TMDBIngestionServiceImpl implements TMDBIngestionService {
 
         for (TMDBShowDTO.TMDBSeasonSummaryDTO seasonDTO : seasonDTOs) {
             Season season = seasonRepository.findById(seasonDTO.getId())
-                    .orElseGet(() -> {
-                        Season newSeason = seasonMapper.toSeason(seasonDTO);
-                        return seasonRepository.save(newSeason);
-                    });
+                    .orElseGet(() -> seasonMapper.toSeason(seasonDTO));
             currentSeasons.add(season);
         }
 
         show.setSeasons(currentSeasons);
+        seasonRepository.saveAll(currentSeasons);
+
     }
 }
